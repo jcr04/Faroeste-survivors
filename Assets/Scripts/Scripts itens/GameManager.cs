e9f1e02b0869cd197c9a916e83f326713775ac77
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2023-03-21 14:09:49 -0300

    add gold catcher

---
+++ Assets/Scripts/Scripts itens/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59c75627ba20dc5f6fa0cd73d914692c12bdcaa32a989c12febf0598a4d8cad5
+size 831
---
+++ Assets/Scripts/Scripts itens/GoldItem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:89dfc08ff30d612401d91b81d2386bd7e6f748d9d1e2ee8c3bb169309c6dd536
+size 454
